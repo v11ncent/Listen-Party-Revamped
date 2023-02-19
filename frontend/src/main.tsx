4bfd00889ed6f9a3ee8b33abd84a491033ebe5b6
Vince <45806875+vince1444@users.noreply.github.com>
2023-02-18 23:25:57 -0500

    Added sass dependency and some basic styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./routes/Root";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,5 +1,5 @@
 const Root = () => {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return <h1>Hello world!</h1>;
 };
 
 export default Root;
