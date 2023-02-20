8b3c9a288860d87f664d25aa18858220d8a21b8d
Vince <45806875+vince1444@users.noreply.github.com>
2023-02-19 22:46:06 -0500

    Added Auth0 Provider functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth0ProviderWithNavigate from "./components/Auth0ProviderWithNavigate";
 import Root from "./routes/Root";
 import Error from "./routes/Error";
 import App from "./routes/App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.scss";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/app",
-        element: <App />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Auth0ProviderWithNavigate>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route path="/app" element={<App />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Auth0ProviderWithNavigate>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- frontend/src/routes/Error.tsx
+++ frontend/src/routes/Error.tsx
@@ -6,16 +6,16 @@ type ErrorObject = {
 };
 
 const Error = () => {
-  const error = useRouteError() as ErrorObject;
-  console.error(error);
+  //   const error = useRouteError() as ErrorObject;
+  //   console.error(error);
 
   return (
     <>
       <h1>Oops! 🤖</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+      {/* <p>
         <i>{error.statusText || error.message}</i>
-      </p>
+      </p> */}
     </>
   );
 };
