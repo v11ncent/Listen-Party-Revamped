996fbb0d0c1b1f717373d42b528ba1bf9430ebe0
Vince <45806875+vince1444@users.noreply.github.com>
2023-02-27 20:24:34 -0500

    Added About.tsx and added lobbiesArray state in Lobbies.tsx

--- frontend/src/components/Lobbies/Lobbies.tsx
+++ frontend/src/components/Lobbies/Lobbies.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 type Lobbies = {
   id: number;
   name: string;
 }[];
 
 const Lobbies = ({ lobbies }: { lobbies: Lobbies }) => {
+  const [lobbiesArray, setLobbiesArray] = useState([]);
   return <h2>Lobbies</h2>;
 };
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth0ProviderWithNavigate from "./components/Auth0ProviderWithNavigate";
-import Root from "./routes/Root";
-import Error from "./routes/Error";
 import App from "./routes/App";
+import Root from "./routes/Root";
+import About from "./routes/About";
+import Lobby from "./components/Lobbies/Lobby/Lobby";
+import Error from "./routes/Error";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -14,6 +16,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <Routes>
           <Route path="/" element={<Root />}>
             <Route path="/app" element={<App />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/lobby/:lobbyId" element={<Lobby />} />
           </Route>
           <Route path="*" element={<Error />} />
         </Routes>
