d1fd690e74b068a87912e74a963dbd64fab1bc11
Vince <45806875+vince1444@users.noreply.github.com>
2023-03-02 20:56:38 -0500

    Developed api and got it working on Postman

--- backend/server.ts
+++ backend/server.ts
@@ -1,15 +1,22 @@
 // https://blog.logrocket.com/how-to-set-up-node-typescript-express/
 
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, Router } from "express";
 import dotenv from "dotenv";
+import { router } from "./routes/lobby";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const lobbyRouter: Router = router;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", lobbyRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("<h1>Express + TypeScript Server</h1>");
 });
 
 app.listen(port, () => {
